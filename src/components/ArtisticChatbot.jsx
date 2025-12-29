6158852f3e723181ea58b98ccc0b3a2fa2f8b421
Rajesh Patel <rajesh.patel@iviewlabs.com>
2025-12-29 11:25:32 +0530

    user avtar

--- src/components/ArtisticChatbot.jsx
+++ src/components/ArtisticChatbot.jsx
@@ -250,7 +250,14 @@ export default function ArtisticChatbot() {
   const messagesEndRef = useRef(null);
   const theme = isDark ? colors.dark : colors.light;
   const user = JSON.parse(localStorage.getItem("user"));
-  const username = user?.username || "User";
+  
+  // const username = user?.username || "User";
+  const username =
+  user.username ||
+  user.name ||
+  user.email?.split("@")[0] ||
+  "User";
+  console.log(username);
   const userInitial = username.charAt(0).toUpperCase();
 
   useEffect(() => {
